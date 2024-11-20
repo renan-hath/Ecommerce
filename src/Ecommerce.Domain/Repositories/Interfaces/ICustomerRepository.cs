f6752eea92560ac01952dae3ec254f0ca32b9d3e
Renan Hatherly <renan.hath@gmail.com>
2024-11-20 16:19:09 -0300

    Repositories interfaces created
    - Repository interfaces for product and customer created.
    - Scaffolding generic classes removed.

---
+++ src/Ecommerce.Domain/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Repositories.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Customer GetById(Guid id);
+    }
+}
---
+++ src/Ecommerce.Domain/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Product GetById(Guid id);
+        IEnumerable<Product> GetAll();
+        void Update(Product product);
+    }
+}
