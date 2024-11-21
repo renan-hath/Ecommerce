69f644536d8a752bbf2b581bf3432e4737c10ad8
Renan Hatherly <renan.hath@gmail.com>
2024-11-20 23:51:49 -0300

    Infrastructure Service Collection Extensions created
    - Created an extension class in Infrastructure so the API is able to call it without breaking the domain-driven-design.

--- src/Ecommerce.API/Program.cs
+++ src/Ecommerce.API/Program.cs
@@ -1,6 +1,16 @@
+using Ecommerce.Application.Services;
+using Ecommerce.Application.Services.Interfaces;
+using Ecommerce.Domain.Repositories.Interfaces;
+using Ecommerce.Infrastructure.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructureServices();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IReservationService, ReservationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/Ecommerce.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Domain.Repositories.Interfaces;
+using Ecommerce.Infrastructure.DataAccess;
+using Ecommerce.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddDbContext<EcommerceDbContext>(options => options.UseInMemoryDatabase("EcommerceDb"));
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IReservationRepository, ReservationRepository>();
+
+            return services;
+        }
+    }
+}
